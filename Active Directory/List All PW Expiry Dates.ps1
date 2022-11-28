bd13834dc77abb98e2ba21355ef694f697b14b64
gfmcgrath <gary.mcgrath@gmail.com>
2022-11-28 13:39:26 +1300

    Renamed file

---
+++ Active Directory/List All PW Expiry Dates.ps1
@@ -0,0 +1,1 @@
+Get-ADUser -Filter {Enabled -eq $True -and PasswordNeverExpires -eq $False} -Properties "DisplayName", "msDS-UserPasswordExpiryTimeComputed" | Select-Object -Property "Displayname",@{Name="ExpiryDate";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}} | Sort-Object ExpiryDate
