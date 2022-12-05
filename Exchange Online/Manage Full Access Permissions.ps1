6e66024ba908ea8b3e1b0093e588018db28cb61b
gfmcgrath <gary.mcgrath@gmail.com>
2022-12-06 11:10:48 +1300

    Consolidate EXO Permissions Management

--- Exchange Online/Manage Full Access Permissions.ps1
+++
@@ -1,9 +0,0 @@
-# View all mailboxes that the specified user has been assigned Full Access permission to
-
-Get-Mailbox | Get-MailboxPermission -User 'gary.mcgrath@millbrook.co.nz'
-
-
-
-# Add Full Access permissions for the specified mailbox to the specified user
-	
-Add-MailboxPermission -Identity "<mailbox>" -User "<user>" -AccessRights FullAccess -InheritanceType All
