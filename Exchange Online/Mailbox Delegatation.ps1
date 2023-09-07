719181884de4be0fed141592834e4c1df0acb4ce
Gary McGrath <gary.mcgrath@gmail.com>
2023-09-07 22:45:52 +1200

    Updated Exchange Online Scripts

--- Exchange Online/Mailbox Delegatation.ps1
+++ Exchange Online/Mailbox Delegatation.ps1
@@ -2,10 +2,10 @@
 ###################################
 
 # View permissions for the specified MAILBOX
-Get-Mailbox -Identity 'user@qldc.govt.nz' | Get-MailboxPermission           #FullAccess
-Get-Mailbox -Identity 'user@qldc.govt.nz' | Get-RecipientPermission         #SendAs
+Get-Mailbox -Identity 'user@domain.com' | Get-MailboxPermission           #FullAccess
+Get-Mailbox -Identity 'user@domain.com' | Get-RecipientPermission         #SendAs
 
-# View permissions for the specified USER (queries ALL mailboxes in domain, can take long time to run)
+# View permissions for the specified USER
 Get-Mailbox -ResultSize Unlimited | Get-MailboxPermission -User 'user@domain.com'
 Get-Mailbox -ResultSize Unlimited | Get-RecipientPermission -Trustee 'user@domain.com'
 
@@ -13,8 +13,9 @@ Get-Mailbox -ResultSize Unlimited | Get-RecipientPermission -Trustee 'user@domai
 
 # ASSIGN PERMISSIONS
 ###################################
+
 # Add Full Access permissions	
-Add-MailboxPermission -Identity "mailbox-to-give-permissions-to@qldc.govt.nz" -User "user-to-receive-permissions@domain.com" -AccessRights FullAccess -InheritanceType All
+Add-MailboxPermission -Identity "mailbox-to-give-permissions-to@domain.com" -User "user-to-receive-permissions@domain.com" -AccessRights FullAccess -InheritanceType All
 
 # Add Send As Permissions
-Add-RecipientPermission "mailbox-to-give-permissions-to@qldc.govt.nz" -AccessRights SendAs -Trustee "user-to-receive-permissions@domain.com" 
+Add-RecipientPermission "mailbox-to-give-permissions-to@domain.com" -AccessRights SendAs -Trustee "user-to-receive-permissions@domain.com" 
